6b8b8b0af4adb36bdfeeb1105ed16ad38818dcf9
juandaalda <juanaldana12354@outlook.com>
2025-11-17 01:40:38 -0600

    Agregando main.js

--- frontend-calc/src/main.js
+++ frontend-calc/src/main.js
@@ -1,24 +1,10 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { setupButtons } from "./modules/buttons.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+setupButtons();
 
-setupCounter(document.querySelector('#counter'))
+// Modo oscuro
+const toggleBtn = document.getElementById("darkmode-toggle");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
